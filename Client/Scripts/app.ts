bd1bb38ec9e85cfb08a988e4a2485ec9bab8cabf
Hanqing1234 <lhq3361951@gmail.com>
2021-09-17 00:21:48 -0400

    added bootstrap and font-awesome support

---
+++ Client/Scripts/app.ts
@@ -0,0 +1,13 @@
+//IIFE
+
+"use strict";
+
+(function(){
+
+    function Start(): void
+    {
+        console.log("App Started");
+    }
+
+    window.addEventListener("load", Start);
+})();
--- app.js
+++ app.js
@@ -17,7 +17,8 @@ app.use((0, morgan_1.default)('dev'));
 app.use(express_1.default.json());
 app.use(express_1.default.urlencoded({ extended: false }));
 app.use((0, cookie_parser_1.default)());
-app.use(express_1.default.static(path_1.default.join(__dirname, 'public')));
+app.use(express_1.default.static(path_1.default.join(__dirname, 'Client')));
+app.use(express_1.default.static(path_1.default.join(__dirname, 'node_modules')));
 app.use('/', index_1.default);
 app.use(function (req, res, next) {
     next((0, http_errors_1.default)(404));
