882e48a938e3b9a4ee50007d51ab40d2cc6460ff
Hanqing1234 <lhq3361951@gmail.com>
2021-10-01 17:38:07 -0400

    added form validate function

--- Client/Scripts/app.js
+++ Client/Scripts/app.js
@@ -1,7 +1,74 @@
 "use strict";
 (function () {
+    function validateForm() {
+        let contactForm = document.forms[0];
+        if (contactForm) {
+            contactForm.noValidate = true;
+            let errorMessage = document.getElementById("errorMessage");
+            let firstName = document.getElementById("firstName");
+            firstName.addEventListener("blur", (event) => {
+                if (firstName.value.length < 1) {
+                    errorMessage.hidden = false;
+                    errorMessage.textContent = "Please enter a Valid First Name with a length of 2 or more characters";
+                }
+                else {
+                    errorMessage.hidden = true;
+                }
+            });
+            let lastName = document.getElementById("lastName");
+            lastName.addEventListener("blur", (event) => {
+                if (lastName.value.length < 1) {
+                    errorMessage.hidden = false;
+                    errorMessage.textContent = "Please enter a Valid Last Name with a length of 2 or more characters";
+                }
+                else {
+                    errorMessage.hidden = true;
+                }
+            });
+            let contactNumber = document.getElementById("contactNumber");
+            contactNumber.addEventListener("blur", (event) => {
+                let number = /^([0-9]){10}$/;
+                if (!number.test(contactNumber.value)) {
+                    errorMessage.hidden = false;
+                    errorMessage.textContent = "Please enter a Valid contact number";
+                }
+                else {
+                    errorMessage.hidden = true;
+                }
+            });
+            let emailAddress = document.getElementById("emailAddress");
+            emailAddress.addEventListener("blur", (event) => {
+                let email = /^\w+([-+.]\w+)*@\w+([-.]\w+)*\.\w+([-.]\w+)*$/;
+                if (!email.test(emailAddress.value)) {
+                    errorMessage.hidden = false;
+                    errorMessage.textContent = "Please enter a Valid email address";
+                }
+                else {
+                    errorMessage.hidden = true;
+                }
+            });
+            let shortMessage = document.getElementById("shortMessage");
+            let submitButton = document.getElementById("submitButton");
+            submitButton.addEventListener("click", (event) => {
+                event.preventDefault();
+                console.log("Submit Button Clicked");
+                console.log("First name input: " + firstName.value);
+                console.log("Last name input: " + lastName.value);
+                console.log("Contact number input: " + contactNumber.value);
+                console.log("Email address input: " + emailAddress.value);
+                console.log("Message input: " + shortMessage.value);
+            });
+        }
+        return false;
+    }
     function Start() {
-        console.log("App Started");
+        let formValidated = validateForm();
+        if (formValidated) {
+            console.log("successfully validated form");
+        }
+        else {
+            console.warn("form not validated - does not exist");
+        }
     }
     window.addEventListener("load", Start);
 })();
--- Client/Scripts/app.ts
+++ Client/Scripts/app.ts
@@ -9,10 +9,119 @@
 
 (function(){
 
-    function Start(): void
+    function validateForm()
     {
-        console.log("App Started");
+        let contactForm = document.forms[0];
+
+        if(contactForm)
+        {
+            contactForm.noValidate = true;
+
+            let errorMessage = <HTMLInputElement>document.getElementById("errorMessage");
+
+            //Validate first name
+            let firstName = <HTMLInputElement>document.getElementById("firstName");
+            firstName.addEventListener("blur", (event) => 
+            {
+                if(firstName.value.length < 1)
+                {                  
+                    errorMessage.hidden = false;
+                    errorMessage.textContent = "Please enter a Valid First Name with a length of 2 or more characters"; 
+                }
+                else
+                {
+                    errorMessage.hidden = true;
+                }
+            });
+
+            //Validate last name
+            let lastName = <HTMLInputElement>document.getElementById("lastName");
+            lastName.addEventListener("blur", (event) => 
+            {
+                if(lastName.value.length < 1)
+                {                  
+                    errorMessage.hidden = false;
+                    errorMessage.textContent = "Please enter a Valid Last Name with a length of 2 or more characters"; 
+                }
+                else
+                {
+                    errorMessage.hidden = true;
+                }
+            });
+
+            //Validate contact number
+            let contactNumber = <HTMLInputElement>document.getElementById("contactNumber");
+            contactNumber.addEventListener("blur", (event) => 
+            {
+                let number = /^([0-9]){10}$/; //Regular expression for contact number
+                
+                if(!number.test(contactNumber.value))
+                {
+                    errorMessage.hidden = false;
+                    errorMessage.textContent = "Please enter a Valid contact number"; 
+                }
+                else
+                {
+                    errorMessage.hidden = true;
+                }
+            });
+
+            //Validate email address
+            let emailAddress = <HTMLInputElement>document.getElementById("emailAddress");
+            emailAddress.addEventListener("blur", (event) => 
+            {
+                let email = /^\w+([-+.]\w+)*@\w+([-.]\w+)*\.\w+([-.]\w+)*$/; // Regular expression for email address
+                
+                if(!email.test(emailAddress.value))
+                {
+                    errorMessage.hidden = false;
+                    errorMessage.textContent = "Please enter a Valid email address"; 
+                }
+                else
+                {
+                    errorMessage.hidden = true;
+                }
+            });
+
+            let shortMessage = <HTMLInputElement>document.getElementById("shortMessage");
+            
+            // creates a "hook" or reference to the button element with an id of "submitButton"
+            let submitButton = document.getElementById("submitButton");
+
+            submitButton.addEventListener("click", (event) =>
+            {
+                event.preventDefault();
+                //Require input information 
+                console.log("Submit Button Clicked");
+                console.log("First name input: " + firstName.value);
+                console.log("Last name input: " + lastName.value);
+                console.log("Contact number input: " + contactNumber.value);
+                console.log("Email address input: " + emailAddress.value);
+                console.log("Message input: " + shortMessage.value);
+            });
+        }
+        return false;
     }
 
+
+    // named function
+    function Start()
+    { 
+
+       let formValidated = validateForm();
+       
+       if(formValidated)
+       {
+        console.log("successfully validated form");
+        
+       }
+       else
+       {
+        console.warn("form not validated - does not exist");
+       }
+
+    } 
+
     window.addEventListener("load", Start);
+
 })();
