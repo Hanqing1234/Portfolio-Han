450d00f3332e83610b47716e427f3429932eda77
Hanqing Liu <lhq3361951@gmail.com>
2022-04-05 14:52:01 -0400

    applied scss style

--- portfolio-Han/src/sections/Home/index.tsx
+++ portfolio-Han/src/sections/Home/index.tsx
@@ -1,7 +1,34 @@
 import React, { FC } from "react";
+import { Col, Row } from "antd";
+import styles from './styles.module.scss';
+
 
 const Home: FC = () => {
-  return <section></section>;
+
+  return <section id="home" className={styles.home}>
+  <Row className={styles.content}>
+    <Col span={24} md={15} className={styles.intro}>
+      <p>My name is Han，</p>
+      <p>
+        a front-end developer，mainly write{" "}
+        <span className={styles.react}>React</span>.
+      </p>
+      <p>Like writing web apps,，</p>
+      <p>
+        Sometimes
+        <span className={styles.sometime}  />
+      </p>
+    </Col>
+
+    <Col span={24} md={9} className={styles.ballWrapper}>
+
+    </Col>
+
+    <Col className={styles.next} span={24}>
+
+    </Col>
+  </Row>
+</section>;
 };
 
 export default Home;
