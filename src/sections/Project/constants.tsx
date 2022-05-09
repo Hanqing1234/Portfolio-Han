7f62a48cffdfead42723b51f1a8effea04026607
Hanqing Liu <lhq3361951@gmail.com>
2022-05-09 17:36:15 -0400

    added a new project to project page

--- src/sections/Project/constants.tsx
+++ src/sections/Project/constants.tsx
@@ -4,9 +4,10 @@ import { ProjectItem } from "./Item";
 import HighLight from "../../components/HighLight";
 
 // linter-guide
-import linterGuideScreenshot from "../../assets/screenshots/tq-system.jpg";
+import tqSite from "../../assets/screenshots/tq-system.jpg";
+import surveySite from "../../assets/screenshots/survey-site.jpg";
 
-import express from "../../assets/icons/express.png";
+import express from "../../assets/icons/express.jpg";
 
 // tech stack
 import react from "../../assets/icons/react.png";
@@ -15,9 +16,11 @@ import css from "../../assets/icons/css.png";
 import js from "../../assets/icons/js.png";
 import ts from "../../assets/icons/ts.png";
 import npm from "../../assets/icons/npm.png";
-import eslint from "../../assets/icons/eslint.png";
+import mern from "../../assets/icons/mern.png";
 import node from "../../assets/icons/node.png";
-import mongo from "../../assets/icons/mongodb.png"
+import mongo from "../../assets/icons/mongodb.png";
+import ejs from "../../assets/icons/ejs.jpg"
+
 
 // links
 import link from "../../assets/svgs/link.svg";
@@ -26,13 +29,12 @@ import github from "../../assets/svgs/github.svg";
 // project
 export const projects: ProjectItem[] = [
   {
-    logo: eslint,
+    logo: mern,
     title: "TQ(technical question) System",
-    content: <Image src={linterGuideScreenshot} />,
+    content: <Image src={tqSite} />,
     description: (
       <div>
         <p>
-       
           A website that customers can submit technical ticket of their product. 
           <br/><br/>
           Registered users can login and process tickets to different user role. 
@@ -51,8 +53,34 @@ export const projects: ProjectItem[] = [
     { content: "NPM", image: npm },
     { content: "CSS", image: css },
     { content: "JavaScript", image: js },
-    { content: "TypeScript", image: mongo },],
-    
+    { content: "MongoDB", image: mongo },],
+  },
+  {
+    logo: express,
+    title: "Survey Management Website",
+    content: <Image src={surveySite} />,
+    description: (
+      <div>
+        <p>
+        A website that registered users can manage surveys.
+          <br/><br/>
+          Anyone can take the survey and the result will be showed to the user who create this survey.
+          <br/><br/>
+          Used <HighLight isDark>EJS template</HighLight> as frontend, <HighLight isDark>Express.js, Node.js, MongoDB</HighLight> as backend.
+        </p>
+      </div>
+    ),
+    links: [
+      { content: "https://survey-site-project.herokuapp.com/", image: link },
+      { content: "https://github.com/Hanqing1234/survey-management-project", image: github }
+    ],
+    techUsed: [{ content: "EJS", image: ejs  },
+    { content: "Node", image: node},
+    { content: "Express", image: express },
+    { content: "NPM", image: npm },
+    { content: "CSS", image: css },
+    { content: "JavaScript", image: js },
+    { content: "MongoDB", image: mongo },],
   },
 
  
